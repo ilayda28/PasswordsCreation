d842c064eee4fd8ae5a14fd61364a049b780a745
İlayda Sena Keleş <ilaydasenakeles28@gmail.com>
2023-07-20 20:08:10 +0300

    created generatedPassword

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
+  password= '';
+
   lenght: number =0;
   includeLetters: boolean = false;
   includeNumbers: boolean = false;
@@ -33,10 +35,41 @@ export class AppComponent {
   }
 
   buttonClick (){
-    console.log (`Değerlerim;
-    Letters : ${this.includeLetters}
-    Numbers : ${this.includeNumbers}
-    Symbols : ${this.includeSymbols}`)
+    const numbers = '1234567890';
+    const letters = 'abcdefghijklmnoprsştuvyz';
+    const symbols= '@#£$?*!&%^'
+
+
+    let validChars= '';// kullanıcıdan alınan bir giriş alanında geçerli karakterleri kısıtlamak veya doğrulamak için kullanılan bir kural veya işlevi
+
+    if (this.includeLetters){
+      validChars+= letters;       //eğer true ise
+    }
+
+    if (this.includeNumbers){
+      validChars+= numbers;
+    }
+
+    if (this.includeSymbols){
+      validChars+= symbols;
+    }
+
+      let generatedPassword =''; //random şifre oluşturuyoruz
+      for (let i=0; i<this.lenght; i++){
+
+        const index = Math.floor(Math.random()* validChars.length) //Math.floor();  bir sayının en küçük tam sayıya doğru yuvarlanmasını sağlar
+        generatedPassword += validChars[index];
+
+    }// örneğin numaraların sembollerin harflerin toplamı 45 ise bu method bize 0dan 44e kadar random şifre verecek
+
+      this.password=generatedPassword;
+      //Math.random 0 ile 1 arasında değer alır ama 1 dahil değil
+
+
+
+
   }
+
+
 
 }
